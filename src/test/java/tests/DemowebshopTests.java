2055e2c6e49f5106d22a5ba3a31cf910112d1877
makakom <max_pain@mail.ru>
2022-03-15 23:16:12 +0300

    Добавил тесты Api

---
+++ src/test/java/tests/DemowebshopTests.java
@@ -0,0 +1,39 @@
+package tests;
+
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class DemowebshopTests {
+
+    @Test
+    void addToCartWithCookieTest() {
+        String cardCount =
+                given()
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                        .cookie("Nop.customer=88f590c6-59e9-4a55-b243-7395b35f0ce2;")
+                        .when()
+                        .get("http://demowebshop.tricentis.com/cart")
+                        .then().extract().body().asString().split("cart-qty\">")[1]
+                        .split("</")[0].replaceAll("[^\\d ]", "");
+
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie("Nop.customer=88f590c6-59e9-4a55-b243-7395b35f0ce2;")
+                .body("product_attribute_72_5_18=53" +
+                        "&product_attribute_72_6_19=54" +
+                        "&product_attribute_72_3_20=57" +
+                        "&addtocart_72.EnteredQuantity=1")
+                .when()
+                .post("http://demowebshop.tricentis.com/addproducttocart/details/72/1")
+                .then()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message", is("The product has been added to your " +
+                        "<a href=\"/cart\">shopping cart</a>"))
+                .body("updatetopcartsectionhtml",
+                        is(String.format("(%d)", Integer.parseInt(cardCount) + 1)));
+    }
+}
